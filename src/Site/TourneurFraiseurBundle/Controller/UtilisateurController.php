6fcf0460f5571a475080dac7b8ee1f364c893340
JennyNg943 <jenny.ng943@gmail.com>
2017-07-04 16:48:57 +0200

    Modification des informations personnelles

---
+++ src/Site/TourneurFraiseurBundle/Controller/UtilisateurController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Site\TourneurFraiseurBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Site\TourneurFraiseurBundle\Form\Utilisateur\RecruteurModificationType;
+use Symfony\Component\HttpFoundation\Request;
+use Site\TourneurFraiseurBundleu\Form\Utilisateur\CandidatModificationType;
+use Site\TourneurFraiseurBundleu\Form\Utilisateur\EmployeurModificationType;
+
+class UtilisateurController extends Controller
+{
+	
+	public function changeAction(Request $request)
+    {
+		$user = $this->getUser();
+		if($user->getType() == "Recruteur" || $user->getType() == "Admin"){
+		$repository = $this->getDoctrine()->getManager()->getRepository('SiteUserBundle:Sy_Recruteur');
+		$utilisateur = $repository->find($user);
+		$form = $this->createForm(RecruteurModificationType::class, $utilisateur);
+		}
+		
+		if($user->getType() == "Employeur"){
+			$repository = $this->getDoctrine()->getManager()->getRepository('SiteUserBundle:Sy_Employeur');
+			$utilisateur = $repository->find($user);
+			$form = $this->createForm(EmployeurModificationType::class, $utilisateur);
+		}	
+		if($user->getType() == "Candidat"){
+			$repository = $this->getDoctrine()->getManager()->getRepository('SiteUserBundle:Sy_Candidature');
+			$utilisateur = $repository->find($user);
+			$form = $this->createForm(CandidatModificationType::class, $utilisateur);
+		}
+		$valide = 0;
+		if ($request->isMethod('POST')) {
+			if ($form->handleRequest($request)->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($utilisateur);
+				$em->flush();
+				$valide = 1;
+			}
+		}
+		
+        return $this->render('SiteTourneurFraiseurBundle:Utilisateur:ModificationInformation.html.twig',array('form'=>$form->createView(),'utilisateur'=>$utilisateur,'valide'=>$valide));
+    }
+	
+	
+}
---
+++ src/Site/TourneurFraiseurBundle/Form/Utilisateur/CandidatModificationType.php
@@ -0,0 +1,89 @@
+<?php
+namespace Site\TourneurFraiseurBundleu\Form\Utilisateur;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Site\TourneurFraiseurBundle\Repository\DepartementRepository;
+use Site\TourneurFraiseurBundle\Repository\SiteRepository;
+
+class CandidatModificationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {	
+        $builder
+		->remove('username')		
+		->add('idCivilite',				EntityType::class,array(
+				'label'			=> 'Civilite* :',
+				'class'			=> 'SiteTourneurFraiseurBundle:Sy_Civilite',
+				'choice_label'	=> 'intitulecivilite',
+			'placeholder'	=> ''
+				
+		))
+		->add('nomCandidat',			TextType::class,array(
+				'label'			=> 'Nom* :',
+				
+		))
+		->add('prenomCandidat',			TextType::class,array(
+				'label'			=> 'Prenom* :',
+				
+		))				
+		->add('villeCandidat',			TextType::class,array(
+				'label'			=> 'Ville :',
+				'required'		=> false
+				
+		))	
+		->add('Departement',				EntityType::class,array(
+			'class'			=> 'SiteTourneurFraiseurBundle:Departement',
+			'label'			=> 'Département* :',
+			'placeholder'	=> '',
+			'query_builder' => function(DepartementRepository $er) {
+								return $er->getDeptTri();}
+		))
+		->add('CP',				TextType::class,array(
+			'label'			=> 'CP* :'
+			))
+		->add('telportcandidat',			IntegerType::class,array(
+				'label'			=> 'Telephone* :',
+				
+		))			
+		->add('site',					EntityType::class,array(
+				'label'=>'Domaine préféré* :',
+				'class'=>'SiteTourneurFraiseurBundle:Sy_Siteemploi',
+				'choice_label'=>'domaine',
+				'placeholder'	=> '',
+				'query_builder' => function(SiteRepository $er) {
+									return $er->getSiteTri();}))		
+		->add('newsletter',				CheckboxType::class,array(
+				'label'=>'Ne pas recevoir d\'offres d\'emploi',
+				'required'=>false))				
+		->add('save', SubmitType::class,array('label'=>'Confirmer mes informations'));
+    
+    }
+	public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => \Site\UserBundle\Entity\Sy_Candidature::class,
+        ));
+    }
+	
+}
+
+
+
+
+
+
+
+
+
+
+?>
