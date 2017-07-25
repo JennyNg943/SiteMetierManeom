ddd76af9f21398b38064481f725e653b8612f474
JennyNg943 <jenny.ng943@gmail.com>
2017-07-25 11:11:35 +0200

    Mise en place d'un formulaire de contact au niveau des pubs

--- src/Site/TourneurFraiseurBundle/Controller/AdminController.php
+++ src/Site/TourneurFraiseurBundle/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace Site\TourneurFraiseurBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Site\TourneurFraiseurBundle\Form\PubType;
+use Site\TourneurFraiseurBundle\Form\FormPubType;
 
 class AdminController extends Controller
 {
@@ -28,6 +29,24 @@ class AdminController extends Controller
         $site = $repository->find(15);
         
         return $this->render('SiteTourneurFraiseurBundle:Admin:AffichagePub.html.twig',array('site'=>$site));
+	}
+	
+	public function formPubAction(Request $request){
+		$form = $this->createForm(FormPubType::class);
+		$repository = $this->getDoctrine()->getManager()->getRepository('SiteTourneurFraiseurBundle:Sy_Siteemploi');
+        $site = $repository->find(15);
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			$message = \Swift_Message::newInstance()
+				->setSubject('Reponse PUB')
+				->setFrom($form->get('Mail')->getData())
+				->setTo('m.lopez@maneom.com')
+				->setBody("Reponse à votre pub de ".$form->get('Nom')->getData()." ".$form->get('Prenom')->getData().". \n Mail : ".$form->get('Mail')->getData().". \n Téléphone : ".$form->get('Tel')->getData().". \n Code Postal : ".$form->get('CodePostal')->getData().". \n Societe : ".$form->get('Societe')->getData())
+				;
+			$this->get('mailer')->send($message);
+			return $this->redirect($site->getPub()->getRedirection());
+		}
+		
+		return $this->render('SiteTourneurFraiseurBundle:Admin:FormPub.html.twig',array('site'=>$site,'form'=>$form->createView()));
 	}
 }
 
---
+++ src/Site/TourneurFraiseurBundle/Form/FormPubType.php
@@ -0,0 +1,32 @@
+<?php
+namespace Site\TourneurFraiseurBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
+class FormPubType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('Nom',	TextType::class)
+			->add('Prenom',	TextType::class)
+			->add('Tel',	TextType::class)
+			->add('Mail', \Symfony\Component\Form\Extension\Core\Type\EmailType::class)
+			->add('CodePostal', \Symfony\Component\Form\Extension\Core\Type\NumberType::class)
+			->add('Societe', TextType::class,array('required' => false,'label'=>'Société (facultatif)'))
+			->add('Confirmer', SubmitType::class)	
+				;
+				
+
+	}				
+
+}
+
+
+?>
